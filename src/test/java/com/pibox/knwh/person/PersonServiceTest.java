e6eb1390a00715275407e9262dd9af858b5d6b92
Valentin Piho <val.piho@gmail.com>
2021-04-04 11:07:24 +0300

    tests

--- src/test/java/com/pibox/knwh/person/PersonServiceTest.java
+++ src/test/java/com/pibox/knwh/person/PersonServiceTest.java
@@ -1,12 +1,19 @@
 package com.pibox.knwh.person;
 
+import com.pibox.knwh.person.exception.BadRequestException;
+import com.pibox.knwh.person.exception.PersonNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,12 +35,54 @@ class PersonServiceTest {
     }
 
     @Test
-    @Disabled
     void addPerson() {
+        Person person = new Person("Val", "val@test.com", Gender.MALE);
+        underTest.addPerson(person);
+
+        ArgumentCaptor<Person> personArgumentCaptor = ArgumentCaptor.forClass(Person.class);
+        verify(personRepository).save(personArgumentCaptor.capture());
+
+        Person capturedPerson = personArgumentCaptor.getValue();
+
+        assertThat(capturedPerson).isEqualTo(person);
     }
 
     @Test
-    @Disabled
-    void deletePerson() {
+    void addPersonWithTakenEmail() {
+        Person person = new Person("Val", "val@test.com", Gender.MALE);
+        underTest.addPerson(person);
+
+        given(personRepository.selectExistsEmail(person.getEmail()))
+                .willReturn(true);
+
+        assertThatThrownBy(() -> underTest.addPerson(person))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + person.getEmail() + " taken");
+    }
+
+    @Test
+    void canDeleteStudent() {
+        // given
+        long id = 10;
+        given(personRepository.existsById(id))
+                .willReturn(true);
+        // when
+        underTest.deletePerson(id);
+
+        // then
+        verify(personRepository).deleteById(id);
+    }
+
+    @Test
+    void willThrowWhenDeleteStudentNotFound() {
+        // given
+        long id = 10;
+        given(personRepository.existsById(id))
+                .willReturn(false);
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.deletePerson(id))
+                .isInstanceOf(PersonNotFoundException.class)
+                .hasMessageContaining("Person with id " + id + " does not exists");
     }
 }
